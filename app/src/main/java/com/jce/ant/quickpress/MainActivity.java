e0313a3537ccce5618e2f7bf79d48e6e176d2752
assafbt <assafbt@gmail.com>
2015-12-21 02:50:32 +0200

    setting button works (moves between activities)

--- app/src/main/java/com/jce/ant/quickpress/MainActivity.java
+++ app/src/main/java/com/jce/ant/quickpress/MainActivity.java
@@ -1,13 +1,24 @@
 package com.jce.ant.quickpress;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private FrameLayout fragmentContainer;
+    Button btnSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +40,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-    }
-}
+
+        btnSettings = (Button) findViewById(R.id.settingsBtn);
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, Settings.class));
+            }
+        });
+
+
+    }//onCreate
+
+
+}//MainActivity
